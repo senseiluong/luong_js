429a97df29cba354f2ec4adf707894db06050706
luong_js <senseiluong@gmail.com>
2020-04-04 15:22:47 +0700

    luong

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('HELLO');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 9000
+
+app.get('/', (req, res) => res.send('Hello World!GAUGAU'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
